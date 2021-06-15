2c32083f769fcd13a40f75b13ee5d5a68362e282
KamalDGRT <kamaldgrt@gmail.com>
2021-06-15 23:09:31 +0530

    feat: make the URLs pretty

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
